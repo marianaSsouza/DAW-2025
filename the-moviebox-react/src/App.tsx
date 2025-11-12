56b281247da1430873ae3b7c5a93f29616b54822
Mariana <marianabatistella3@gmail.com>
2025-11-12 09:45:28 -0300

    12-11-2025 | migracao highlight

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/layout/header"
 import { Footer } from "./components/layout/footer"
 import { Navbar } from "./components/layout/navbar"
 import { MovieContent } from "./cases/movies/components/movie-content"
+import Highlight from "./components/layout/highlight"
 
 
 
@@ -12,9 +13,11 @@ function App() {
   return (
     <> 
       <Header />
+      
       <main>
         <Navbar />
-        
+        <Highlight />
+
         <MovieContent />
 
       </main>
